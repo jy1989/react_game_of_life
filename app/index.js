b04632101f35e593f37d82adb4718031ec97f6d5
chenjianyi <github@chenjianyi.com>
2016-12-25 18:26:17 +0800

    commit

--- app/index.js
+++ app/index.js
@@ -4,44 +4,76 @@ import ReactDOM from 'react-dom';
 class Cell extends React.Component {
     constructor(props) {
         super(props);
-    }
+		let cellName=this.props.name;
+		let w=this.props.size;
+		let h=this.props.size;
+        let x=parseInt(cellName.split('_')[0])*w;
+		let y=parseInt(cellName.split('_')[1])*h;
+		
+		this.style={left:x,top:y,width:w,height:h};
+    }/*
     shouldComponentUpdate(nextProps, nextState) {
         return nextProps.alive !== this.props.alive;
-    }
+    }*/
 
     render() {
-        let className = ['cell'];
-        if (this.props.alive) {
-            className.push('alive');
-        }
+        //let className = ['cell'];
+		
+		
+        //if (this.props.alive) {
+            //className.push('alive');
+        //}
 
         return (
-            <div className={className.join(' ')}></div>
+            <div style={this.style} className={'cell alive'}></div>
         );
     }
 }
 
-let cellSize = 2;
-let w = 200;
-let h = 200;
-let chance = 0.5;
-let stopTime = 5000;
+
 class App extends React.Component {
     constructor(props) {
         super(props);
+		this.state={cellSize:2,w:200,h:200,chance:0.5,stopTime:5000,timeLoop:500,run:true}
+		
+    }
+	changeRunState(){
+		//e.stopPropagation();
+		
+		this.setState({run:!this.state.run});
+		
+	}
+    render() {
+		return ( <div>
+			
+			<CellBoard {...this.state} />
+			<button onClick={this.changeRunState.bind(this)}>{this.state.run?'停止':'运行'}</button>
+		</div>
+		)
+	}
+}
+
+
+
+class CellBoard extends React.Component {
+    constructor(props) {
+        super(props);
+		
         this.state = {};
 		this.cellsNeighbor={};
         //this.cellsDiv=[];
         //console.log(store);
         this.checkAlive = this.checkAlive.bind(this);
+        
         this.tick = this.tick.bind(this);
         this.runTime = 1;
         this.aliveCount = 0;
         this.deadCount = 0;
         this.cellsCount = 0;
+		this.run=true;
         //per_width = per_width < 5 ? 10 : per_width;
-        for (let i = 0; i < parseInt(h / cellSize); i++) {
-            for (let j = 0; j < parseInt(w / cellSize); j++) {
+        for (let i = 0; i < parseInt(this.props.h / this.props.cellSize); i++) {
+            for (let j = 0; j < parseInt(this.props.w / this.props.cellSize); j++) {
                 let neighborCells = [];
                 neighborCells.push((i - 1) + '_' + (j - 1));
                 neighborCells.push((i - 1) + '_' + (j + 1));
@@ -56,7 +88,7 @@ class App extends React.Component {
                 this.state[i + '_' + j]['alive'] = (Math.random() > chance);
                 this.state[i + '_' + j]['neighbor'] = neighborCells;
                 */
-				this.state[i + '_' + j] = (Math.random() > chance);
+				this.state[i + '_' + j] = (Math.random() > this.props.chance);
 				this.cellsNeighbor[i + '_' + j] = neighborCells;
 				this.cellsCount++;
             }
@@ -108,11 +140,14 @@ class App extends React.Component {
         Perf.printWasted();
         */
     }
-
+	
 
     tick() {
-        //console.log(this.runTime,stopTime);
-        if (this.runTime >= stopTime) {
+		if(!this.props.run){
+			return;
+		}
+		
+        if (this.runTime >= this.props.stopTime) {
             clearInterval(this.timer);
         }
 
@@ -136,7 +171,7 @@ class App extends React.Component {
     }
 
     componentDidMount() {
-        this.timer = setInterval(this.tick, 100);
+        this.timer = setInterval(this.tick, this.props.timeLoop);
 
     }
 
@@ -149,14 +184,21 @@ class App extends React.Component {
         return (
             <div>
 				<div id="show">
-					{Object.keys(this.state).map((k, index) => <Cell key={k} alive={this.state[k]}/>) }
+					{
+						Object.keys(this.state).map((k, index) =>{ 
+							if(this.state[k]){
+								return <Cell size={this.props.cellSize} key={k} name={k} />
+							}
+						}) 
+					}
 				</div>
 				<div className="detail">
 					<div>细胞数:{this.cellsCount}</div>
-				    <div>剩余进化次数:{(stopTime-this.runTime)}</div>
+				    <div>剩余进化次数:{(this.props.stopTime-this.runTime)}</div>
 				    <div>存活细胞数量:{this.aliveCount}</div>
 				    <div>死亡细胞数量:{this.deadCount}</div>
 				</div>
+				
 			</div>
         );
 
